74243ad530e4baeb85d4b907d6a332427da9e7a7
MarquisAlexander <51330232+MarquisAlexander@users.noreply.github.com>
2019-10-03 20:36:35 -0300

    aplicação mobile

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+import logo from "../assets/logo.png";
+
+export default function Login() {
+    return <View>
+        <Image source={logo} />
+        <Text>
+            EU FALEI QUE PERSISTENCIA TRAZ A CORREÇÃO DO ERRO
+        </Text>
+    </View>
+}
