dfcf9c97c68836db94b9231c7dcc86f54c40fb4b
MarquisAlexander <51330232+MarquisAlexander@users.noreply.github.com>
2019-10-07 16:36:24 -0300

    Atualização do app OBS:problema de conexão com o backend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,28 @@
-import React from "react";
-import { View, KeyboardAvoidingView, Platform, Image, StyleSheet, TextInput, Text, TouchableOpacity } from "react-native";
+import React, {useState} from "react";
+import { View, AsyncStorage, KeyboardAvoidingView, Platform, Image, StyleSheet, TextInput, Text, TouchableOpacity } from "react-native";
+
+import api from '../services/api';
 
 import logo from "../assets/logo.png";
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [techs, setTechs] = useState('');
+
+    async function handleSubmit() {
+        const response = await api.post('/sessions', {
+        email
+    })
+
+    const { _id } = response.data;
+
+    await AsyncStorage.setItem("user", _id);
+    await AsyncStorage.setItem("techs", techs);
+
+    navigation.navigate( 'List' );
+    }
+
+
     return <KeyboardAvoidingView enabled={Platform.OS === "ios"} behavior="padding" style={styles.container}>
         <Image source={logo} />
         <View style={styles.form}>
@@ -15,18 +34,22 @@ export default function Login() {
                 keyboardType="email-address"
                 autoCapitalize="none"
                 autoCorrect={false}
-            />
+                value={email}
+                onChangeText={setEmail}
+                />
 
             <Text style={styles.label}>Tecnologias*</Text>
             <TextInput 
-            style={styles.input}
-            placeholder="Tecnologias de interrese"
-            placeholderTextColor="#999"
-            autoCapitalize="words"
-            autoCorrect={false}
+                style={styles.input}
+                placeholder="Tecnologias de interrese"
+                placeholderTextColor="#999"
+                autoCapitalize="words"
+                autoCorrect={false}
+                value={techs}
+                onChangeText={setTechs}
             />
 
-           <TouchableOpacity style={styles.button}>
+           <TouchableOpacity onPress={handleSubmit} style={styles.button}>
                <Text style={styles.buttonText}>Encontrar spots</Text>
             </TouchableOpacity> 
         </View>
