679cb8b8d98b76133e9ad46e7ccd943883a75cb2
MarquisAlexander <51330232+MarquisAlexander@users.noreply.github.com>
2019-10-14 17:55:34 -0300

    inicializando o desenvolvimento da página list

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,16 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, AsyncStorage } from 'react-native';
+import { SafeAreaView, StyleSheet, AsyncStorage, Image } from 'react-native';
+
+import SpotList from "../components/SpotList";
+
+import logo from "../assets/logo.png"
 
 export default function List() {
+//buscando as tecnologias salvas no momento do login
     const [techs, setTechs] = useState([]);
 
     useEffect(() => {
+//buscando as tecnologias salvas no momento do login
         AsyncStorage.getItem('techs').then(storagedTechs  => {
+                                    //tech.trim tira os espaços do inicio e do final das palavras
             const techsArray = storagedTechs.split(',').map(tech => tech.trim());
             
             setTechs(techsArray);
         })
     }, []);
 
-    return <Text>{techs}</Text>
+    return (
+// logo no topo da tela
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+
+            <SpotList />
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: "contain",
+        alignSelf: "center",
+        marginTop: 50,
+    }
+});
